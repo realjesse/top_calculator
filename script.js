95acbe30446f90f80882755cc292d064798127c7
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-02 19:53:13 -0700

    Create basic checkEventId() functionality

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ calculatorNode.addEventListener('click', event => {
         console.log("button click!");
     }
 })
+
+function checkClass(event) {
+    id = event.target.id;
+    if (id === "0") {
+        null
+    }
+    else if (id === "1") {
+        null
+    }
+    else if (id === "2") {
+        null
+    }
+    else if (id === "3") {
+        null
+    }
+    else if (id === "4") {
+        null
+    }
+    else if (id === "5") {
+        null
+    }
+    else if (id === "6") {
+        null
+    }
+    else if (id === "7") {
+        null
+    }
+    else if (id === "8") {
+        null
+    }
+    else if (id === "9") {
+        null
+    }
+}
