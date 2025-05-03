72701e14c07d3931a850166640fccb46566d46f7
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-03 06:30:10 -0700

    Replace if else statement with switch for checkId

--- script.js
+++ script.js
@@ -13,35 +13,38 @@ calculatorNode.addEventListener('click', checkEventId)
 
 function checkEventId(event) {
     id = event.target.id;
-    if (id === "0") {
-        updateCurrentExpression("0");
-    }
-    else if (id === "1") {
-        updateCurrentExpression("1");
-    }
-    else if (id === "2") {
-        updateCurrentExpression("2");
-    }
-    else if (id === "3") {
-        updateCurrentExpression("3");
-    }
-    else if (id === "4") {
-        updateCurrentExpression("4");
-    }
-    else if (id === "5") {
-        updateCurrentExpression("5");
-    }
-    else if (id === "6") {
-        updateCurrentExpression("6");
-    }
-    else if (id === "7") {
-        updateCurrentExpression("7");
-    }
-    else if (id === "8") {
-        updateCurrentExpression("8");
-    }
-    else if (id === "9") {
-        updateCurrentExpression("9");
+
+    switch (id) {
+        case "0":
+            updateCurrentExpression("0");
+            break;
+        case "1":
+            updateCurrentExpression("1");
+            break;
+        case "2":
+            updateCurrentExpression("2");
+            break;
+        case "3":
+            updateCurrentExpression("3");
+            break;
+        case "4":
+            updateCurrentExpression("4");
+            break;
+        case "5":
+            updateCurrentExpression("5");
+            break;
+        case "6":
+            updateCurrentExpression("6");
+            break;
+        case "7":
+            updateCurrentExpression("7");
+            break;
+        case "8":
+            updateCurrentExpression("8");
+            break;
+        case "9":
+            updateCurrentExpression("9");
+            break;
     }
 }
 
